51da1fafd171a109f2e549bbecab6a6e0d4baf39
ZHAO-Zirui <garry@azurflow.com>
2024-04-12 06:41:11 +0800

    FEAT: add GnssData basic implement

---
+++ carla_utils/core/data/GnssData.py
@@ -0,0 +1,32 @@
+import carla
+
+from .SensorData import SensorData
+
+
+class GnssData(SensorData):
+    """
+    A class to store GNSS data.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.altitude = 0.0
+        self.latitude = 0.0
+        self.longitude = 0.0
+
+    @classmethod
+    def from_carla_measurements(cls, measurements: carla.GnssMeasurement) -> 'GnssData':
+        """
+        Load data from a carla.SensorData instance.
+        :param measurements: cara.SensorData instance
+        :return: SensorData instance
+        """
+        data = cls()  # type: GnssData
+        data = cls.initialize_sensor_basic_data(data, measurements)
+
+        # special imu data
+        data.altitude = float(measurements.altitude)
+        data.latitude = float(measurements.latitude)
+        data.longitude = float(measurements.longitude)
+
+        return data
--- carla_utils/core/data/__init__.py
+++ carla_utils/core/data/__init__.py
@@ -2,10 +2,12 @@ from .SensorData import SensorData
 from .VehicleStatusData import VehicleStatusData
 from .ImageData import ImageData
 from .ImuData import ImuData
+from .GnssData import GnssData
 
 __all__ = [
     'SensorData',
     'VehicleStatusData',
     'ImageData',
-    'ImuData'
+    'ImuData',
+    'GnssData',
 ]
