e3c5de650a7ee8a5022bf3a992f3b83e557858ef
ZHAO-Zirui <garry@azurflow.com>
2024-04-12 06:42:36 +0800

    FEAT: add GnssDataUdp basic implement

--- carla_utils/proxy/__init__.py
+++ carla_utils/proxy/__init__.py
@@ -2,11 +2,13 @@ from .BaseProxy import BaseProxy
 from .ProxyVehicleKeyboardControlPygame import ProxyVehicleKeyboardControlPygame
 from .ProxyCameraDisplayPygame import ProxyCameraDisplayPygame
 from .ProxyImuDataUdp import ProxyImuDataUdp
+from .ProxyGnssDataUdp import ProxyGnssDataUdp
 
 
 __all__ = [
     'BaseProxy',
     'ProxyVehicleKeyboardControlPygame',
     'ProxyCameraDisplayPygame',
-    'ProxyImuDataUdp'
+    'ProxyImuDataUdp',
+    'ProxyGnssDataUdp',
 ]
