003dda87297396ba9976dcea50b76d5291afc3c0
ZHAO-Zirui <garry@azurflow.com>
2024-04-11 22:21:38 +0800

    FEAT: add Camera basic implement

--- carla_utils/actor/__init__.py
+++ carla_utils/actor/__init__.py
@@ -1,9 +1,11 @@
 from .Actor import Actor
 from .Sensor import Sensor
 from .Vehicle import Vehicle
+from .Camera import Camera
 
 __all__ = [
     'Actor',
     'Sensor',
     'Vehicle',
+    'Camera',
 ]
